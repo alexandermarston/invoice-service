eeb5c8e6cbac1955b4e9e70cee55a68aaefde613
Alex Marston <alexander.marston@gmail.com>
2019-08-07 17:52:29 +0000

    Add Bugsnag integration

--- index.js
+++ index.js
@@ -5,13 +5,21 @@ const logger = require('./lib/logger');
 const expressWinston = require('express-winston');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
-const mainRouter = require('./routes/main')
+const bugsnag = require('@bugsnag/js');
+const bugsnagExpress = require('@bugsnag/plugin-express');
+const mainRouter = require('./routes/main');
+
+const bugsnagClient = bugsnag(process.env.BUGSNAG_KEY)
+bugsnagClient.use(bugsnagExpress)
 
 // Set Express Port
 const port = 3000 | process.env.PORT
 
 // Initialise express
 const app = express()
+
+// Initialise the bugsnag middleware
+const middleware = bugsnagClient.getPlugin('express');
 
 // Boom response objects in Express.
 app.use(boom());
@@ -21,6 +29,12 @@ app.use(bodyParser.json());
 
 // Use the Express Validator module
 app.use(expressValidator());
+
+// Handle errors in downstream middleware
+app.use(middleware.requestHandler);
+
+// Handle errors in Express
+app.use(middleware.errorHandler);
 
 // Use the Main Router
 app.use('/', mainRouter);
